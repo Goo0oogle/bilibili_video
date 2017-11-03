613e134e96e65b92f69c57d2ac8ac1bda04247a1
julien-elodie <2644756656@qq.com>
2017-11-03 09:46:40 +0800

    init project

--- bilibili/spiders/bilibili.py
+++ bilibili/spiders/bilibili.py
@@ -2,6 +2,8 @@
 import psycopg2
 import scrapy
 
+from bilibili.items import BilibiliItem
+
 # database
 db = psycopg2.connect(
     database='videoinfodb',
@@ -21,29 +23,18 @@ class BilibiliSpider(scrapy.Spider):
 
     def parse(self, response):
         select = scrapy.Selector(response)
-        Title = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[1]/h1/text()').extract()
-        Date = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[3]/time/i/text()').extract()
-        Plays = select.xpath('//*[@id="dianji"]/text()').extract()
-        Comments = select.xpath('//*[@id="dm_count"]/text()').extract()
-        Coins = select.xpath('//*[@id="v_ctimes"]/text()').extract()
-        Collects = select.xpath('//*[@id="stow_count"]/text()').extract()
-        Videosrc = select.xpath('/html/body/div[4]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[5]/video/@src').extract()
-        Content = select.xpath('//*[@id="v_desc"]/text()').extract()
-        Imgsrc = select.xpath('/html/body/img/@src').extract()
-        Username = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/a[1]/text()').extract()
-        Userimgsrc = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[1]/a[1]/img/@src').extract()
-        Usercontent = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[2]/text()').extract()
-        print(Videosrc)
-        """print(Title)
-        print(Date)
-        print(Plays)
-        print(Comments)
-        print(Coins)
-        print(Collects)
-        print(Videosrc)
-        print(Content)
-        print(Imgsrc)
-        print(Username)
-        print(Userimgsrc)
-        print(Usercontent)"""
+        item = BilibiliItem()
+        item['Title'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[1]/h1/text()').extract()[0]
+        item['Date'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[3]/time/i/text()').extract()[0]
+        item['Plays'] = select.xpath('//*[@id="dianji"]/text()').extract()[0]
+        item['Comments'] = select.xpath('//*[@id="dm_count"]/text()').extract()[0]
+        item['Coins'] = select.xpath('//*[@id="v_ctimes"]/text()').extract()[0]
+        item['Collects'] = select.xpath('//*[@id="stow_count"]/text()').extract()[0]
+        # item['Videosrc'] = select.xpath('/html/body/div[4]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[5]/video/@src').extract()[0]
+        item['Content'] = select.xpath('//*[@id="v_desc"]/text()').extract()[0]
+        item['Imgsrc'] = 'https:' + select.xpath('/html/body/img/@src').extract()[0]
+        item['Username'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/a[1]/text()').extract()[0]
+        item['Userimgsrc'] = 'https:' + select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[1]/a[1]/img/@src').extract()[0]
+        item['Usercontent'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[2]/text()').extract()[0]
+        return item
         
