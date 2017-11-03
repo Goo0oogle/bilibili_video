3e89912415d6dc6edd1b7ef5b2f133a366f3d48c
julien-elodie <2644756656@qq.com>
2017-11-03 20:07:55 +0800

    init project

--- bilibili/pipelines.py
+++ bilibili/pipelines.py
@@ -4,8 +4,70 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import psycopg2
 
 
 class BilibiliPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class PostgrePipeline(object):
+    def __init__(self, *args):
+        super(PostgrePipeline, self).__init__(*args)
+        print('Postgresql is starting...')
+
+    def open_spider(self, spider):
+        self.db = psycopg2.connect(
+            database='videoinfodb',
+            user='ubuntu',
+            password='wyq2644756656',
+            host='111.230.15.157',
+            port='5432'
+        )
+        print('Database is connecting...')
+        self.cursor = self.db.cursor()
+        print('Cursor is creating...')
+    
+    def close_spider(self, spider):
+        self.db.commit()
+        print('Changes has been committed...')
+        self.db.close()
+        print('Database is closing...')
+
+    def process_item(self, item, spider):
+        print('processing items...') 
+        self.cursor.execute(
+            "insert into videoinfo\
+            (\
+                id,\
+                title,\
+                date,\
+                plays,\
+                comments,\
+                coins,\
+                collects,\
+                videosrc,\
+                content,\
+                imgsrc,\
+                username,\
+                userimgsrc,\
+                usercontent,\
+            ) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            %(
+                item['Id'],
+                item['Title'],
+                item['Date'],
+                item['Plays'],
+                item['Comments'],
+                item['Coins'],
+                item['Collects'],
+                item['Videosrc'],
+                item['Content'],
+                item['Imgsrc'],
+                item['Username'],
+                item['Userimgsrc'],
+                item['Usercontent']
+            )
+        )
+        print('items has been processed...')
--- bilibili/spiders/bilibili.py
+++ bilibili/spiders/bilibili.py
@@ -1,40 +1,63 @@
 # -*- coding: utf-8 -*-
-import psycopg2
-import scrapy
+from scrapy import Selector
+from scrapy import Request
+from scrapy.spiders import Spider
+import re
 
 from bilibili.items import BilibiliItem
 
-# database
-db = psycopg2.connect(
-    database='videoinfodb',
-    user='ubuntu',
-    password='wyq2644756656',
-    host='111.230.15.157',
-    port='5432'
-)
-
-class BilibiliSpider(scrapy.Spider):
+class BilibiliSpider(Spider):
     name = 'bilibili'
     allowed_domains = ['bilibili.com']
     start_urls = [
-        # "https://www.bilibili.com/"
-        "https://www.bilibili.com/video/av15924319/"
+        "https://www.bilibili.com/"
+        # "https://www.bilibili.com/video/av15924319/"
     ]
 
     def parse(self, response):
-        select = scrapy.Selector(response)
+        select = Selector(response)
+        items_urls = select.xpath('//*[@id="primary_menu"]/ul/li/a/@href').extract()
+        for items_url in items_urls:
+            yield Request(
+                url='https:' + items_url,
+                callback=self.parse_item,
+            )
+
+    def parse_item(self, response):
+        select = Selector(response)
+        item_urls = select.xpath('//a[@target="_blank"]/@href').extract()
+        av = re.compile('//www.bilibili.com/video/(av.*)/')
+        for item_url in item_urls:
+            if av.match(item_url):
+                Id = av.split(item_url)[1]
+                yield Request(
+                    url= 'https:' + item_url,
+                    meta={
+                        'Id': Id,
+                    },
+                    callback=self.parse_details,
+                )
+
+    def parse_details(self, response):
+        select = Selector(response)
         item = BilibiliItem()
+        item['Id'] = response.meta['Id']
         item['Title'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[1]/h1/text()').extract()[0]
         item['Date'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div[3]/time/i/text()').extract()[0]
-        item['Plays'] = select.xpath('//*[@id="dianji"]/text()').extract()[0]
-        item['Comments'] = select.xpath('//*[@id="dm_count"]/text()').extract()[0]
-        item['Coins'] = select.xpath('//*[@id="v_ctimes"]/text()').extract()[0]
-        item['Collects'] = select.xpath('//*[@id="stow_count"]/text()').extract()[0]
+        Plays = select.xpath('//*[@id="viewbox_report"]/div[1]/div[4]/div[1]/@title').extract()[0]
+        item['Plays'] = re.findall('([0-9]+)', Plays)[0]
+        Comments = select.xpath('//*[@id="viewbox_report"]/div[1]/div[4]/div[2]/@title').extract()[0]
+        item['Comments'] = re.findall('[0-9]+', Comments)[0]
+        Coins = select.xpath('//*[@id="viewbox_report"]/div[1]/div[4]/div[4]/@title').extract()[0]
+        item['Coins'] = re.findall('[0-9]+', Coins)[0]
+        Collects = select.xpath('//*[@id="viewbox_report"]/div[1]/div[4]/div[5]/@title').extract()[0]
+        item['Collects'] = re.findall('[0-9]+', Collects)[0]
         # item['Videosrc'] = select.xpath('/html/body/div[4]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[5]/video/@src').extract()[0]
+        item['Videosrc'] = 'TODO'
         item['Content'] = select.xpath('//*[@id="v_desc"]/text()').extract()[0]
         item['Imgsrc'] = 'https:' + select.xpath('/html/body/img/@src').extract()[0]
         item['Username'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/a[1]/text()').extract()[0]
         item['Userimgsrc'] = 'https:' + select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[1]/a[1]/img/@src').extract()[0]
         item['Usercontent'] = select.xpath('/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[2]/text()').extract()[0]
-        return item
+        yield item
         
