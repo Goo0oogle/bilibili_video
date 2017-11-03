1ea8b014c1cebe23520b808f1543180e9ae09845
julien-elodie <2644756656@qq.com>
2017-11-03 22:56:38 +0800

    init project

--- bilibili/pipelines.py
+++ bilibili/pipelines.py
@@ -55,7 +55,7 @@ class PostgrePipeline(object):
                 )
             )
         except psycopg2.ProgrammingError as err:
-            print('raise an error:' + err.message)
+            print('raise an error:' + err)
         else:    
             self.db.commit()
         print('items has been processed...')
