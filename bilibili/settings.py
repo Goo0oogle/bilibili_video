6b9d7c5c7d25b81474832ad351eb20df0579863d
julien-elodie <2644756656@qq.com>
2017-11-05 12:46:28 +0800

    init project

--- bilibili/settings.py
+++ bilibili/settings.py
@@ -91,7 +91,7 @@ COOKIES_DEBUG = False
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'bilibili.middlewares.BilibiliSpiderMiddleware': 543,
+   'bilibili.middlewares.BilibiliSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
